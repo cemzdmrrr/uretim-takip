2721a0c5fe9679415f77e229da711d673465fab5
cemzdmr <cemmozdemirr.34@gmail.com>
2026-04-20 19:53:16 +0300

    build: web release - anasayfa yeni tasarim

--- build/web/flutter_bootstrap.js
+++ build/web/flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"78fc3012e45889657f72359b005af7beac47ba
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2148060039"
+    serviceWorkerVersion: "3006728556"
   }
 });
