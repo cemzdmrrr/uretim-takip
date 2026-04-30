66512ac1ccf50166c3fa3360987f337b33b90fdd
cemzdmr <cemmozdemirr.34@gmail.com>
2026-05-01 02:59:01 +0300

    Build web release

--- build/web/flutter_bootstrap.js
+++ build/web/flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"78fc3012e45889657f72359b005af7beac47ba
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "438498514"
+    serviceWorkerVersion: "2602737303"
   }
 });
