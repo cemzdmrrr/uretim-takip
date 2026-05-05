b9c4d1105b783a0ee63c68532b9d800400b7030c
cemzdmrrr <cem.ozdemir@akarorme.com>
2026-05-05 22:51:18 +0300

    fix: yetki sistemi, detay dialog, splash routing, uretim panel erisim

--- build/web/flutter_bootstrap.js
+++ build/web/flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"78fc3012e45889657f72359b005af7beac47ba
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1345809677"
+    serviceWorkerVersion: "2328060826"
   }
 });
